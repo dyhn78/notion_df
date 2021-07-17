ff944e7285c0ce1704189eddbf1b91f2f804ce63
dyhn_Zenbook <dyhn78@gmail.com>
2021-07-17 15:17:15 +0900

    env 파일

--- terminal.py
+++ terminal.py
@@ -1,8 +1,6 @@
 from notion_py.applications.log_editor.request import connect_to_naljja, connect_to_gigan
 from notion_py.applications.read_scraper import update_ilggi
-import os
 
-print(os.environ.get('NOTION_TOKEN'))
 update_ilggi()
 connect_to_naljja(check_only_past_x_days=7)
 connect_to_gigan(check_only_past_x_days=7)
