b9f030f22fa0300c5a274cff9ab0dc0e6c847e2a
dyhn_Zenbook <dyhn78@gmail.com>
2021-07-23 20:48:43 +0900

    lib_snu를 셀레늄으로 다시 짜기

--- notion_py/applications/read_scraper/lib_snu.py
+++ notion_py/applications/read_scraper/lib_snu.py
@@ -1,13 +1,12 @@
 from urllib import parse
-import requests
-from bs4 import BeautifulSoup
 from selenium.common.exceptions import NoSuchElementException
 
 from .lib_gy import retry_webdriver, try_twice, SeleniumScraper
 
 
 class SNULibrary(SeleniumScraper):
-    tag_lib_info = 'div.result-item-text.layout-fill.layout-column.flex > div.search-result-availability-line-wrapper ' \
+    tag_lib_info = 'div.result-item-text.layout-fill.layout-column.flex > ' \
+                   'div.search-result-availability-line-wrapper ' \
                    '> prm-search-result-availability-line > div > div > button '
 
     @try_twice
@@ -15,15 +14,17 @@ class SNULibrary(SeleniumScraper):
     def execute(self, book_name: str) -> str:
 
         parsedname = parse.quote(book_name)
-        url = f'https://primoapac01.hosted.exlibrisgroup.com/primo-explore/search?query=any,contains,{parsedname}&tab=all' \
+        url = f'https://primoapac01.hosted.exlibrisgroup.com/primo-explore/search?query=any,contains,' \
+              f'{parsedname}&tab=all' \
               f'&search_scope=ALL&vid=82SNU&mfacet=rtype,include,print_book,1&mfacet=library,exclude,DPT,' \
               f'1&mfacet=library,exclude,HD_CHEM_BIO,1&mfacet=library,exclude,HD_DIGIT,1&mfacet=library,exclude,KYU,' \
               f'1&mfacet=library,exclude,MUSEUM,1&offset=0&pcAvailability=false '
 
+        print(url)
         driver = self.drivers[0]
         driver.start_client()
         driver.get(url)
-        driver.implicitly_wait(3)
+        driver.implicitly_wait(15)
         lib_info = ''
         try:
             lib_info = driver.find_element_by_css_selector(self.tag_lib_info).text
@@ -35,6 +36,9 @@ class SNULibrary(SeleniumScraper):
 
 
 """
+import requests
+from bs4 import BeautifulSoup
+
 @try_twice
 def scrap_snu_library(book_name) -> str:
     parsedname = parse.quote(book_name)
