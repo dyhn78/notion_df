dddf3b7c79440ba63dd06481a5267af9774f5088
dyhn_Zenbook <dyhn78@gmail.com>
2021-09-24 17:36:24 +0900

    .

--- terminal_main.py
+++ terminal_main.py
@@ -3,7 +3,7 @@ from notion_py.applications.prop_matcher import PropertyMatcher
 from notion_py.applications.media_scraper import MediaScraper
 
 PropertyMatcher(date_range=7).execute()
-MediaScraper().execute(page_size=0)
+MediaScraper().execute(page_size=5)
 
 stopwatch('작업 완료')
 # close_program = input("작업 완료. 아무 키나 누르십시오...")
