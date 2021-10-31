5b892afbf3adb7280ebff8ce0916f28f3b4d9e3d
dyhn_Zenbook <dyhn78@gmail.com>
2021-10-31 22:51:51 +0900

    .

--- editor_test.py
+++
@@ -1,9 +0,0 @@
-from notion_py.interface import RootEditor
-
-root = RootEditor()
-page = root.open_inline_page('56ce1823-d086-4a05-a85f-88e47ece7f72')
-"""block = page.attachments.create_text_block()
-block.contents.write_to_do('some_text', True)"""
-subpage = page.attachments.create_page_item()
-subpage.contents.write_title('xyz')
-page.save()
