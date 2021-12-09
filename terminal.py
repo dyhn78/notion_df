3288834eb1aa5ed3b561e53761416960102f2197
Zenbook <dyhn78@gmail.com>
2021-12-09 12:58:44 +0900

    .

--- terminal.py
+++ terminal.py
@@ -4,11 +4,12 @@ import datetime as dt
 from notion_zap.apps.routines import main
 
 
-with open('debug.log', 'w') as log:
-    try:
-        log.write(f"last execution: {dt.datetime.now()}")
-        main.execute()
-    except Exception as err:
-        traceback.print_exc(file=log)
-        raise err
+log = open('debug.log', 'w', encoding='utf-8')
+log.write(f"last execution: {dt.datetime.now()}")
+try:
+    main.execute()
+except Exception as err:
+    traceback.print_exc(file=log)
+    log.close()
+    raise err
 # x = input("아무 키나 누르십시오...")
