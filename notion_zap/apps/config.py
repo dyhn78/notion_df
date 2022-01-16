c0de27a6fe6d1e1ec38a449391994f5ef137e0a4
dyhn78 <dyhn78@gmail.com>
2022-01-16 14:49:40 +0900

    .

--- notion_zap/apps/config.py
+++ notion_zap/apps/config.py
@@ -4,7 +4,7 @@ class DatabaseInfo:
     DATES = ('날짜', '961d1ca0a3d24a46b838ba85e710f18d')
     JOURNALS = ('일지', 'c8d46c01d6c941a9bf8df5d115a05f03')
     WRITINGS = ('쓰기', '069bbebd632f4a6ea3044575a064cf0f')
-    SCHEDULES = ('목표', 'c226cffe6cf84ab996bbc384bf26bf1d')
+    SCHEDULES = ('계획', 'c226cffe6cf84ab996bbc384bf26bf1d')
     TASKS = ('요점', 'e8782fe4e1a34c9d846d57b01a370327')
 
     PROJECTS = ('실행', 'eb2f09a1de41412e8b2357bc04f26e74')
