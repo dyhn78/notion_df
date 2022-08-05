f5fa02d9721f2e96dcb8a751cf0bb13c72a423f4
dyhn78 <dyhn78@gmail.com>
2022-08-05 20:45:10 +0900

    Update my_block.py

--- notion_zap/apps/my_block.py
+++ notion_zap/apps/my_block.py
@@ -21,15 +21,15 @@ class MyBlock(Enum):
     projects = ('활동', '69b4e66c4ee43b6a5e40c8b28e6f9d1', EmojiCode.DIRECT_HIT)
 
     journals = ('진행', 'c8d46c01d6c941a9bf8df5d115a05f03', EmojiCode.PURPLE_CIRCLE)
-    targets = ('요점', 'fa7d93f6fbd341f089b185745c834811', EmojiCode.PURPLE_HEART)
+    targets = ('처리', 'fa7d93f6fbd341f089b185745c834811', EmojiCode.PURPLE_HEART)
 
     issues = ('줄기', 'e8782fe4e1a34c9d846d57b01a370327', EmojiCode.BLUE_CIRCLE)
-    notes = ('바탕', '069bbebd632f4a6ea3044575a064cf0f', EmojiCode.BLUE_HEART)
+    notes = ('결실', '069bbebd632f4a6ea3044575a064cf0f', EmojiCode.BLUE_HEART)
 
     readings = ('읽기', 'c326f77425a0446a8aa309478767c85b', EmojiCode.YELLOW_CIRCLE)
     points = ('꼭지', '2c5411ba6a0f43a0a8aa06295751e37a', EmojiCode.YELLOW_HEART)
 
-    streams = ('실행', 'eb2f09a1de41412e8b2357bc04f26e74', EmojiCode.RED_CIRCLE)
+    streams = ('전개', 'eb2f09a1de41412e8b2357bc04f26e74', EmojiCode.RED_CIRCLE)
     groups = ('갈래', '679c2515870d46e3a107b42cd2a5ffc3', EmojiCode.RED_HEART)
 
     domains = ('주제', 'eca1ba6d4831459ca8becc283f1f8c4e', EmojiCode.ORANGE_CIRCLE)
