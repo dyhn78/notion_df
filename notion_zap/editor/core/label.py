2ddfce56de13f9e281e9d5fcd4bd11a765e94a63
Zenbook <dyhn78@gmail.com>
2022-08-14 19:21:33 +0900

    re-add "LabelKey" confirm test cases

--- notion_zap/editor/core/label.py
+++ notion_zap/editor/core/label.py
@@ -1,62 +1,63 @@
 from __future__ import annotations
 
+import logging
 from enum import Enum
-from typing import Iterable, cast, Any, Optional
+from typing import Iterable, Optional
 
 
-class Label(Enum):
-    # TODO: change "superlabels" arg to "Self" after python 3.11
-    def __init__(self, description: Any, *superlabels: Label | Iterable[Label]):
-        print(self, [f"({type(label)}: {label})" for label in superlabels])
-        if not description:
-            pass  # TODO: fill the description with str(self)
-        self._value_ = description
-        self.superlabels: set[Label] = set()
+class Label:
+    # TODO: change "supers" arg to "Self" after python 3.11
+    def __init__(self, *superlabels: Label | Iterable[Label]):
         superlabel_values = self._parse_superlabel_input(superlabels)
+        logging.debug(f"{self=}, {superlabels=}, {superlabel_values=}")
+
+        self.supers: set[Label] = set()
         for superlabel in superlabel_values:
             self._recursively_add_superlabels(superlabel)
 
+        self.key: Optional[LabelKey] = None
+
     def _recursively_add_superlabels(self, superlabel: Label):
-        if superlabel in self.superlabels:
+        if superlabel in self.supers:
             return
-        self.superlabels.add(superlabel)
-        for super_superlabel in superlabel.superlabels:
+        self.supers.add(superlabel)
+        for super_superlabel in superlabel.supers:
             self._recursively_add_superlabels(super_superlabel)
 
     def _parse_superlabel_input(self, superlabel_input: tuple[Label | Iterable[Label]]) -> Iterable[Label]:
-        cls = type(self)
-
-        def parse_label_input(label_key: Label | Any) -> Optional[Label]:
-            if isinstance(label_key, cls):
-                return label_key
-            try:
-
-                return cls[label_key]
-            except KeyError:
-                pass
-            return None
-
-        def is_labels_iterable(label_keys) -> Optional[list[Label]]:
+        def is_labels_iterable(elements) -> Optional[list[Label]]:
             _labels = []
-            for label_key in label_keys:
-                print(f"\t{label_keys=}, {label_key=}")
-                if label := parse_label_input(label_key):
-                    _labels.append(label)
+            for element in elements:
+                if isinstance(element, Label):
+                    _labels.append(element)
                 else:
                     return None
             return _labels
 
-        try:
-            if (labels := is_labels_iterable(superlabel_input)) is not None:
-                return labels
-            if len(superlabel_input) == 1 and (labels := is_labels_iterable(superlabel_input[0])) is not None:
-                return labels
-            labels = "ERROR"
-            raise ValueError
-            # raise ValueError(f"{self=}, {superlabel_input=}")
-        finally:
-            print(f"{self=}, {superlabel_input=}, {labels=}")
+        if (labels := is_labels_iterable(superlabel_input)) is not None:
+            return labels
+        if len(superlabel_input) == 1 and (labels := is_labels_iterable(superlabel_input[0])) is not None:
+            return labels
+        raise ValueError(f"{self=}, {superlabel_input=}")
+
+
+class LabelKey(Enum):
+    def __init__(self, value: Label):
+        self._value_ = value
+        if value.key is not None:
+            raise ValueError(f"{self=}, {value=}")
+        value.key = self
+
+    def __str__(self):
+        args = [self.name]
+        if supers := self.supers:
+            args.append(f"supers={', '.join(sorted(superkey.name for superkey in supers))}")
+        return f"{type(self).__name__}({', '.join(args)})"
 
     @property
     def value(self) -> Label:
-        return cast(Label, super().value)
+        return self._value_
+
+    @property
+    def supers(self) -> set[LabelKey]:
+        return {value.key for value in self.value.supers if value.key is not None}
--- tests/editor/core/test_label.py
+++ tests/editor/core/test_label.py
@@ -1,16 +1,34 @@
-from notion_zap.editor.core.label import Label
+from notion_zap.editor.core.label import Label, LabelKey
 
 
-class MyLabel(Label):
-    a = 'aa'
-    b = 'bb'
-    c = 'cc', a
-    d = 'dd', b, c
-    e = 'ee', c, d
+class MyLabel:
+    a = Label()
+    b = Label()
+    c = Label(a)
+    d = Label(b, c)
+    e = Label(c, d)
+
+
+class MyLabelKey(LabelKey):
+    aa = MyLabel.a
+    bb = MyLabel.b
+    cc = MyLabel.c
+    dd = MyLabel.d
+    ee = MyLabel.e
+
+
+def test_label():
+    assert [getattr(MyLabel, key).supers for key in "abcde"] == [
+        set(), set(), {MyLabel.a}, {MyLabel.a, MyLabel.b, MyLabel.c},
+        {MyLabel.a, MyLabel.b, MyLabel.c, MyLabel.d}
+    ]
 
 
 def test_label_enum():
-    assert [label.superlabels for label in MyLabel] == [
-        set(), set(), {MyLabel.a}, {MyLabel.a, MyLabel.b, MyLabel.c},
-        {MyLabel.a, MyLabel.b, MyLabel.c, MyLabel.d}
+    assert [str(getattr(MyLabelKey, key)) for key in ("aa", "bb", "cc", "dd", "ee")] == [
+        "MyLabelKey(aa)",
+        "MyLabelKey(bb)",
+        "MyLabelKey(cc, supers=aa)",
+        "MyLabelKey(dd, supers=aa, bb, cc)",
+        "MyLabelKey(ee, supers=aa, bb, cc, dd)"
     ]
