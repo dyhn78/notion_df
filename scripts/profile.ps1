d828dfbf2e83aa52d37435aca1f53612beef1b7c
Zenbook <dyhn78@gmail.com>
2022-08-14 16:36:40 +0900

    update ide configs

--- scripts/profile.ps1
+++ scripts/profile.ps1
@@ -1,2 +1,4 @@
-"Activate Notion-py/venv"
-C:\Workspace\Notion-py\venv\Scripts\activate.ps1
+# put this script on $PSHOME
+
+"Activate notion_zap\venv"
+C:\Workspace\notion_zap\venv\Scripts\activate.ps1
