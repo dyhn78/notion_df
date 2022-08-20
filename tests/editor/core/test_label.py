1ac55acbb13cd3a3fda200524def9bc2150705b9
dyhn78 <dyhn78@gmail.com>
2022-08-20 16:47:41 +0900

    implementing label again
    https://www.notion.so/dyhn/0814-Label-038a5ed492fe4e5b8067d5e3c19ea0ae

--- tests/editor/core/test_label.py
+++ tests/editor/core/test_label.py
@@ -1,49 +1,13 @@
-from notion_zap.editor.core.label import Label, LabelEnum
+from notion_zap.editor.core.label import Label
 
 
-class MyLabel:
-    a = Label()
-    b = Label()
-    c = Label(a)
-    d = Label(b, c)
-    e = Label(c, d)
-
-
-class MyLabelEnum(LabelEnum):
-    aa = MyLabel.a
-    bb = MyLabel.b
-    cc = MyLabel.c
-    dd = MyLabel.d
-    ee = MyLabel.e
-
-
-class MyLabelEnum2(LabelEnum):
-    a = Label()
-    b = Label()
-    c = Label(a)
-    d = Label(b, c)
-    e = Label(c, d)
+class MyLabel(Label):
+    a = ''
+    b = 0
+    c = 'a'
+    d = 'a', 'b'
 
 
 def test_label():
     assert [getattr(MyLabel, key).supers for key in "abcde"] == [
-        set(), set(), {MyLabel.a}, {MyLabel.a, MyLabel.b, MyLabel.c},
-        {MyLabel.a, MyLabel.b, MyLabel.c, MyLabel.d}
-    ]
-
-
-def test_label_enum():
-    assert [str(getattr(MyLabelEnum, key)) for key in ("aa", "bb", "cc", "dd", "ee")] == [
-        "MyLabelEnum(aa)",
-        "MyLabelEnum(bb)",
-        "MyLabelEnum(cc; supers=aa)",
-        "MyLabelEnum(dd; supers=aa, bb, cc)",
-        "MyLabelEnum(ee; supers=aa, bb, cc, dd)"
-    ]
-    assert [str(getattr(MyLabelEnum2, key)) for key in "abcde"] == [
-        "MyLabelEnum2(a)",
-        "MyLabelEnum2(b)",
-        "MyLabelEnum2(c; supers=a)",
-        "MyLabelEnum2(d; supers=a, b, c)",
-        "MyLabelEnum2(e; supers=a, b, c, d)"
-    ]
+        set(), set(), {MyLabel.a}, {MyLabel.a, MyLabel.b, MyLabel.c}]
