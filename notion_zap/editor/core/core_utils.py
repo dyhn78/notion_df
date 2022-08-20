1d4a299c6cc7a9f9748d3da7511463b81c9959e7
Zenbook <dyhn78@gmail.com>
2022-08-20 18:13:18 +0900

    finish implementation remove old features

--- notion_zap/editor/core/core_utils.py
+++ notion_zap/editor/core/core_utils.py
@@ -2,4 +2,4 @@ from typing import Iterable
 
 
 def repr_object(self, args: Iterable[str]) -> str:
-    return f"{type(self).__name__}({'; '.join(args)})"
+    return f"{type(self).__name__}({', '.join(args)})"
---
+++ notion_zap/editor/core/exceptions.py
@@ -0,0 +1,11 @@
+# TODO: think cooler project name
+from abc import ABC
+
+
+class NotionZapException(Exception, ABC):
+    pass
+
+
+class CircularLabelException(NotionZapException):
+    """circular hierarchy between labels detected; in other words, broken DAG."""
+    pass
