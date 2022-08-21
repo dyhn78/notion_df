114f6361d9ba5f21d793303f402e6e9f5fca231b
Zenbook <dyhn78@gmail.com>
2022-08-21 16:08:32 +0900

    rename Property->Field
    notion://www.notion.so/0821-Field-08c9adb9ec014594b3eb329b89bcc33b

--- notion_zap/editor/core/block.py
+++ notion_zap/editor/core/block.py
@@ -3,45 +3,60 @@
 # 용어 정리: "Block" 이라 하면 (BaseBlock, PageBlock, PageRow, ...) 를 모두 가리킨다
 from __future__ import annotations
 
-from typing import Type
+from typing import Type, Any, cast
 
-from notion_zap.editor.core.entity import EntityMeta, BaseProperty, BaseEntity
+from notion_zap.editor.core.entity import EntityMeta, BaseField, BaseEntity, Entity
+from notion_zap.editor.core.utils import NotionZapException
 
 
 class BaseBlockMeta(EntityMeta):
-    pass
+    def __new__(mcs, name, bases, namespace: dict[str, Any]):
+        cls = cast(Type[Entity], type.__new__(mcs, name, bases, namespace))
+        for key, value in namespace.items():
+            if isinstance(value, BaseField):
+                if not any(isinstance(value, allowed_property_type)
+                           for allowed_property_type in cls.allowed_property_types):
+                    raise PropertyTypeException(name, key, value)
+        return cls
 
 
-class BlockId(BaseProperty[str]):
+class PropertyTypeException(NotionZapException):
+    """the block type does not allow this type of property."""
+
+    def __init__(self, block_type_name: str, block_property_name: str, block_property_value: BaseField):
+        self.args = (f"{block_type_name}.{block_property_name}: {type(block_property_value).__name__}",)
+
+
+class BlockId(BaseField[str]):
     """real id on the server"""
     ...
 
 
-class TempId(BaseProperty[str]):
+class TempId(BaseField[str]):
     """temporary identification for yet-not-created blocks"""
     ...
 
 
-class Title(BaseProperty[str]):
+class Title(BaseField[str]):
     """available on PageBlock"""
     ...
 
 
-class Field(BaseProperty):
+class PageRowProperty(BaseField):
     """available on PageRow"""
     ...
 
 
-class DateField(Field):
+class DateProperty(PageRowProperty):
     ...
 
 
-class RelationField(Field):
+class RelationProperty(PageRowProperty):
     ...
 
 
 class BaseBlock(BaseEntity, metaclass=BaseBlockMeta):
-    allowed_property_types: list[Type[BaseProperty]] = []
+    allowed_property_types: list[Type[BaseField]] = []
     id = BlockId()
     temp_id = TempId()
 
--- notion_zap/editor/core/entity.py
+++ notion_zap/editor/core/entity.py
@@ -3,18 +3,11 @@ from __future__ import annotations
 from abc import abstractmethod
 from typing import Type, Any, cast, TypeVar, Generic, Optional
 
-from notion_zap.editor.core.utils import NotionZapException
-
 
 class EntityMeta(type):
     # TODO: ABCMeta.__call__
     def __new__(mcs, name, bases, namespace: dict[str, Any]):
         cls = cast(Type[Entity], type.__new__(mcs, name, bases, namespace))
-        for key, value in namespace.items():
-            if isinstance(value, BaseProperty):
-                if not any(isinstance(value, allowed_property_type)
-                           for allowed_property_type in cls.allowed_property_types):
-                    raise PropertyTypeException(name, key, value)
         return cls
 
 
@@ -26,32 +19,25 @@ class BaseEntity(metaclass=EntityMeta):
 
 
 Entity = TypeVar('Entity', bound=BaseEntity)
-PropertyValue = TypeVar('PropertyValue')
+FieldValue = TypeVar('FieldValue')
 
 
-class BaseProperty(Generic[PropertyValue, Entity]):
-    def __init__(self, default_value: Optional[PropertyValue] = None):
+class BaseField(Generic[FieldValue, Entity]):
+    def __init__(self, default_value: Optional[FieldValue] = None):
         """None default_value means no default value. 
         remind that you cannot actually store null value on Notion."""  # TODO: better documentation
-        self.data: dict[Entity, PropertyValue] = {}  # TODO: naming. candidate: 'column', 'spectrum'
-        self.default_value: Optional[PropertyValue] = default_value
+        self.data: dict[Entity, FieldValue] = {}  # TODO: naming. candidate: 'column', 'spectrum'
+        self.default_value: Optional[FieldValue] = default_value
 
-    def __get__(self, instance: Optional[Entity], owner: Type[Entity]) -> PropertyValue:
+    def __get__(self, instance: Optional[Entity], owner: Type[Entity]) -> FieldValue:
         if instance is None:
             return self.default_value
         if self.default_value is None:
             return self.data[instance]
         return self.data.get(instance, self.default_value)
 
-    def __set__(self, instance: Optional[Entity], value: PropertyValue):
+    def __set__(self, instance: Optional[Entity], value: FieldValue):
         if instance is None:
             self.default_value = value
         else:
             self.data[instance] = value
-
-
-class PropertyTypeException(NotionZapException):
-    """the block type does not allow this type of property."""
-
-    def __init__(self, block_type_name: str, block_property_name: str, block_property_value: BaseProperty):
-        self.args = (f"{block_type_name}.{block_property_name}: {type(block_property_value).__name__}",)
