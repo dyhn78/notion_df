e94bae773d2e91886cf80ebb9ad298219ffcc28c
Younghoon YUN <dyhn78@gmail.com>
2022-12-29 21:58:41 +0900

    Update myblock.py

--- notion_zap/apps/myblock.py
+++ notion_zap/apps/myblock.py
@@ -19,11 +19,10 @@ class MyBlock(Enum):
     dates = ('일간', '961d1ca0a3d24a46b838ba85e710f18d', EmojiCode.GREEN_CIRCLE)
     weeks = ('주간', 'd020b399cf5947a59d11a0b9e0ea45d0', EmojiCode.GREEN_HEART)
 
-    processes = ('진행', 'c8d46c01d6c941a9bf8df5d115a05f03', EmojiCode.ORANGE_CIRCLE)
-    events = ('일과', 'c226cffe6cf84ab996bbc384bf26bf1d', EmojiCode.ORANGE_HEART)
+    processes = ('진행', 'c8d46c01d6c941a9bf8df5d115a05f03', EmojiCode.ORANGE_HEART)
+    events = ('일지', 'c226cffe6cf84ab996bbc384bf26bf1d', EmojiCode.ORANGE_CIRCLE)
 
     journals = ('바탕', 'fa7d93f6fbd341f089b185745c834811', EmojiCode.WHITE_CIRCLE)
-    issues = ('주관', '8baa30054f00483a8f0538fd3cd9543a', EmojiCode.BLUE_CIRCLE)
     tasks = ('과제', 'e8782fe4e1a34c9d846d57b01a370327', EmojiCode.BLUE_HEART)
 
     readings = ('읽기', 'c326f77425a0446a8aa309478767c85b', EmojiCode.YELLOW_CIRCLE)
@@ -40,6 +39,7 @@ class MyBlock(Enum):
     locations = ('장소', '920e2e10225d450d8bb084697f6d0fc6', EmojiCode.BLACK_HEART)
     writings = ('표현', '069bbebd632f4a6ea3044575a064cf0f', EmojiCode.BLACK_HEART)
     statuses = ('생활', '52df5fae504b4d35918b03bb82080500', EmojiCode.BLACK_HEART)
+    issues = ('주관', '8baa30054f00483a8f0538fd3cd9543a', EmojiCode.WHITE_CIRCLE)
 
 
 if __name__ == '__main__':
