e6fae4e2b9685a2835cdb245720c86c01bbfaefd
Younghoon YUN <dyhn78@gmail.com>
2023-01-01 21:14:48 +0900

    Update myblock.py

--- notion_zap/apps/myblock.py
+++ notion_zap/apps/myblock.py
@@ -19,11 +19,11 @@ class MyBlock(Enum):
     dates = ('일간', '961d1ca0a3d24a46b838ba85e710f18d', EmojiCode.GREEN_CIRCLE)
     weeks = ('주간', 'd020b399cf5947a59d11a0b9e0ea45d0', EmojiCode.GREEN_HEART)
 
-    processes = ('진행', 'c8d46c01d6c941a9bf8df5d115a05f03', EmojiCode.ORANGE_HEART)
+    processes = ('진행', 'c8d46c01d6c941a9bf8df5d115a05f03', EmojiCode.BLUE_CIRCLE)
     events = ('일지', 'c226cffe6cf84ab996bbc384bf26bf1d', EmojiCode.ORANGE_CIRCLE)
 
-    journals = ('바탕', 'fa7d93f6fbd341f089b185745c834811', EmojiCode.WHITE_CIRCLE)
-    tasks = ('과제', 'e8782fe4e1a34c9d846d57b01a370327', EmojiCode.BLUE_HEART)
+    journals = ('바탕', 'fa7d93f6fbd341f089b185745c834811', EmojiCode.ORANGE_HEART)
+    tasks = ('꼭지', 'e8782fe4e1a34c9d846d57b01a370327', EmojiCode.BLUE_HEART)
 
     readings = ('읽기', 'c326f77425a0446a8aa309478767c85b', EmojiCode.YELLOW_CIRCLE)
     points = ('마디', '2c5411ba6a0f43a0a8aa06295751e37a', EmojiCode.YELLOW_HEART)
