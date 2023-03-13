775088b25be95784c1717a048674576fdd42302d
Younghoon YUN <dyhn78@gmail.com>
2023-03-13 21:58:49 +0900

    update config

--- notion_zap/apps/myblock.py
+++ notion_zap/apps/myblock.py
@@ -14,24 +14,23 @@ class MyBlock(Enum):
     @property
     def prefix_title(self):
         return self.prefix + self.title
-
-    themes = ('주제', '5464267393e940a58e3f10db306bf3e4', EmojiCode.PURPLE_CIRCLE)
-    topics = ('중심', 'eca1ba6d4831459ca8becc283f1f8c4e', EmojiCode.PURPLE_HEART)
-
-    dates = ('일간', '961d1ca0a3d24a46b838ba85e710f18d', EmojiCode.GREEN_CIRCLE)
-    weeks = ('주간', 'd020b399cf5947a59d11a0b9e0ea45d0', EmojiCode.GREEN_HEART)
+    
+    processes = ('일과', 'c8d46c01d6c941a9bf8df5d115a05f03', EmojiCode.BLUE_CIRCLE)
+    tasks = ('진행', 'e8782fe4e1a34c9d846d57b01a370327', EmojiCode.BLUE_HEART)
 
     journals = ('일지', 'c226cffe6cf84ab996bbc384bf26bf1d', EmojiCode.ORANGE_CIRCLE)
     notes = ('바탕', 'fa7d93f6fbd341f089b185745c834811', EmojiCode.ORANGE_HEART)
 
-    processes = ('일과', 'c8d46c01d6c941a9bf8df5d115a05f03', EmojiCode.BLUE_CIRCLE)
-    issues = ('정리', '9f21ad86079d4caaa7ed9461a7f37288', EmojiCode.BLUE_HEART)
-
-    streams = ('줄기', 'eb2f09a1de41412e8b2357bc04f26e74', EmojiCode.RED_CIRCLE)
-    tasks = ('꼭지', 'e8782fe4e1a34c9d846d57b01a370327', EmojiCode.RED_HEART)
+    points = ('꼭지', '2c5411ba6a0f43a0a8aa06295751e37a', EmojiCode.YELLOW_HEART)
+    topics = ('줄기', 'eca1ba6d4831459ca8becc283f1f8c4e', EmojiCode.PURPLE_HEART)
 
     readings = ('읽기', 'c326f77425a0446a8aa309478767c85b', EmojiCode.YELLOW_CIRCLE)
-    points = ('마디', '2c5411ba6a0f43a0a8aa06295751e37a', EmojiCode.YELLOW_HEART)
+    
+    streams = ('전개', 'eb2f09a1de41412e8b2357bc04f26e74', EmojiCode.RED_CIRCLE)
+    issues = ('마디', '9f21ad86079d4caaa7ed9461a7f37288', EmojiCode.RED_HEART)
+    
+    dates = ('일간', '961d1ca0a3d24a46b838ba85e710f18d', EmojiCode.GREEN_CIRCLE)
+    weeks = ('주간', 'd020b399cf5947a59d11a0b9e0ea45d0', EmojiCode.GREEN_HEART)
 
     projects = ('활동', '69b4e66c4ee43b6a5e40c8b28e6f9d1', EmojiCode.BLACK_HEART)
     people = ('인물', '3c08cdba5a044e9c9b7e31ee8509f506', EmojiCode.BLACK_HEART)
@@ -41,6 +40,7 @@ class MyBlock(Enum):
     statuses = ('생활', '52df5fae504b4d35918b03bb82080500', EmojiCode.BLACK_HEART)
     issues_depr = ('주관', '8baa30054f00483a8f0538fd3cd9543a', EmojiCode.BLACK_HEART)
     groups = ('갈래', '679c2515870d46e3a107b42cd2a5ffc3', EmojiCode.BLACK_HEART)
+    themes = ('주제', '5464267393e940a58e3f10db306bf3e4', EmojiCode.BLACK_HEART)
 
 
 if __name__ == '__main__':
