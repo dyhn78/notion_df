fdc223843d0a368a35a42a814fcbb17489d3aeb2
Younghoon YUN <dyhn78@gmail.com>
2023-05-21 22:16:49 +0900

    Update run_workflow.py

--- run_workflow.py
+++ run_workflow.py
@@ -1,4 +1,4 @@
 from workflow.run import Workflow
 
 if __name__ == '__main__':
-    Workflow(print_body=False, create_window=False).run_window()
+    Workflow(print_body=False, create_window=False).run_all()  # TODO: fix to run_window()
