7d466d456c5acc7b6533277055920511a297ee67
dyhn78 <dyhn78@gmail.com>
2023-05-22 09:39:28 +0900

    fix

--- run_workflow.py
+++ run_workflow.py
@@ -1,4 +1,4 @@
 from workflow.run import Workflow
 
 if __name__ == '__main__':
-    Workflow(print_body=False, create_window=False).run_from_last_execution()
+    Workflow(print_body=True, create_window=False).run_from_last_execution()
