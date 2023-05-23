6ab2424e24643cc70f48b7ecf1e3a356bc2486e4
dyhn78 <dyhn78@gmail.com>
2023-05-23 11:20:12 +0900

    code clean run_workflow.py

--- run_workflow.py
+++ run_workflow.py
@@ -6,17 +6,18 @@ from time import sleep
 import psutil
 
 
-def is_already_running(script_name):
+def is_already_running(script_path: Path):
     count = 0
     for process in psutil.process_iter(['name', 'cmdline']):
-        if script_name in process.cmdline():
+        if script_path in process.cmdline():
             count += 1
     return count > 1
 
 
 if __name__ == '__main__':
-    if is_already_running(os.path.basename(__file__)):
-        print("Script is already running.")
+    path = Path(__file__)
+    if is_already_running(path):
+        sys.stderr.write("Script is already running.")
         sys.exit(1)
     print(f'{"#" * 5 } Start.')
 
@@ -24,11 +25,11 @@ if __name__ == '__main__':
 
     log_enabled = run_from_last_success(print_body=False, create_window=False)
     print(f'{"#" * 5 } {"Done." if log_enabled else "No new record."}')
-    if is_already_running(os.path.basename(__file__)):
-        print("Script is already running.")
+    if is_already_running(path):
+        sys.stderr.write("Other script is running.")
         sys.exit(1)
     sleep(5)
     try:
-        os.execv(sys.executable, [sys.executable, (Path(__file__).resolve())])
+        os.execv(sys.executable, [sys.executable, (path.resolve())])
     except OSError as e:
         print("Execution failed:", e)
