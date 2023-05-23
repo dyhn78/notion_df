32716e75e4d1d62bf3e573f758cbbff78424c6a6
Younghoon YUN <dyhn78@gmail.com>
2023-05-23 10:00:29 +0900

    Update run_workflow.py

--- run_workflow.py
+++ run_workflow.py
@@ -24,6 +24,9 @@ if __name__ == '__main__':
 
     log_enabled = run_from_last_success(print_body=False, create_window=False)
     print(f'{"#" * 5 } {"Done." if log_enabled else "No new record."}')
+    if is_already_running(os.path.basename(__file__)):
+        print("Script is already running.")
+        sys.exit(1)
     sleep(5)
     try:
         os.execv(sys.executable, [sys.executable, (Path(__file__).resolve())])
