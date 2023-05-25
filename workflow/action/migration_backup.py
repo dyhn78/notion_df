b9cd75fe96ede5a6b6a000e2e5ea2e11bc05a415
dyhn78 <dyhn78@gmail.com>
2023-05-25 19:22:00 +0900

    fix

--- workflow/action/migration_backup.py
+++ workflow/action/migration_backup.py
@@ -136,11 +136,16 @@ class MigrationBackupLoadAction(IterableAction):
             return pick('🟡관계')
         if this_db_enum == DatabaseEnum.reading_db and linked_db_enum == DatabaseEnum.event_db:
             return pick('🔵관계')
-        if this_db_enum in [DatabaseEnum.issue_db, DatabaseEnum.stream_db] and linked_db_enum == DatabaseEnum.stream_db:
+        if this_db_enum in DatabaseEnum.issue_db and linked_db_enum == DatabaseEnum.stream_db:
             if this_prev_db_enum in [DatabaseEnum.topic_db, DatabaseEnum.subject_db]:
                 return pick('🔴요소')
             else:
                 return pick('🔴구성')
+        if this_db_enum in DatabaseEnum.stream_db and linked_db_enum == DatabaseEnum.stream_db:
+            if this_prev_db_enum in [DatabaseEnum.topic_db, DatabaseEnum.subject_db]:
+                return pick('🔴요소')
+            else:
+                return pick('🔴관계')
 
         # default cases
         if linked_db_enum:
