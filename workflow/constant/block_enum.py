3a9839f83227a3b34d4ed111a94d058302932812
Younghoon YUN <dyhn78@gmail.com>
2023-05-29 18:04:45 +0900

    Update block_enum.py

--- workflow/constant/block_enum.py
+++ workflow/constant/block_enum.py
@@ -38,16 +38,16 @@ class DatabaseEnum(Enum):
     note_db = ('바탕', 'fa7d93f6fbd341f089b185745c834811', EmojiCode.ORANGE_HEART)
 
     event_db = ('일과', 'c8d46c01d6c941a9bf8df5d115a05f03', EmojiCode.BLUE_CIRCLE)
-    session_db = ('마디', '9f21ad86079d4caaa7ed9461a7f37288', EmojiCode.BLUE_HEART)
+    issue_db = ('진행', 'e8782fe4e1a34c9d846d57b01a370327', EmojiCode.BLUE_HEART)
 
     reading_db = ('읽기', 'c326f77425a0446a8aa309478767c85b', EmojiCode.YELLOW_CIRCLE)
-    agenda_db = ('담론', 'eca1ba6d4831459ca8becc283f1f8c4e', EmojiCode.YELLOW_HEART)
+    division_db = ('자산', '52d387ea0aaa470cb69332708c61b34d', EmojiCode.YELLOW_HEART)  # TODO -> document
 
-    stream_db = ('전개', 'eb2f09a1de41412e8b2357bc04f26e74', EmojiCode.RED_CIRCLE)
-    issue_db = ('꼭지', 'e8782fe4e1a34c9d846d57b01a370327', EmojiCode.RED_HEART)
+    stream_db = ('전개', 'eb2f09a1de41412e8b2357bc04f26e74', EmojiCode.RED_CIRCLE)  # TODO -> stem
+    session_db = ('마디', '9f21ad86079d4caaa7ed9461a7f37288', EmojiCode.RED_HEART)
 
     topic_db = ('주제', '2c5411ba6a0f43a0a8aa06295751e37a', EmojiCode.PURPLE_CIRCLE)
-    division_db = ('분과', '52d387ea0aaa470cb69332708c61b34d', EmojiCode.PURPLE_HEART)
+    agenda_db = ('담론', 'eca1ba6d4831459ca8becc283f1f8c4e', EmojiCode.PURPLE_HEART)
 
     date_db = ('일간', '961d1ca0a3d24a46b838ba85e710f18d', EmojiCode.GREEN_CIRCLE)
     week_db = ('주간', 'd020b399cf5947a59d11a0b9e0ea45d0', EmojiCode.GREEN_HEART)
