a9d7af7547c811b3d69a24d06269e7fe01ba6902
Younghoon YUN <dyhn78@gmail.com>
2023-06-27 12:23:11 +0900

    Update block_enum.py

--- workflow/constant/block_enum.py
+++ workflow/constant/block_enum.py
@@ -12,21 +12,21 @@ from workflow.constant.emoji_code import EmojiCode
 
 _id_to_member = {}
 
-
+# TODO
 class DatabaseEnum(Enum):
     event_db = ('일과', 'c8d46c01d6c941a9bf8df5d115a05f03', EmojiCode.BLUE_CIRCLE)
     issue_db = ('줄기', 'e8782fe4e1a34c9d846d57b01a370327', EmojiCode.BLUE_HEART)
 
     journal_db = ('일지', 'c226cffe6cf84ab996bbc384bf26bf1d', EmojiCode.ORANGE_CIRCLE)
     stage_db = ('바탕', 'fa7d93f6fbd341f089b185745c834811', EmojiCode.ORANGE_HEART)
-
-    topic_db = ('분야', '52d387ea0aaa470cb69332708c61b34d', EmojiCode.YELLOW_CIRCLE)
-    reading_db = ('읽기', 'c326f77425a0446a8aa309478767c85b', EmojiCode.YELLOW_HEART)
-
-    stream_db = ('진행', 'eb2f09a1de41412e8b2357bc04f26e74', EmojiCode.RED_CIRCLE)
+    
+    stream_db = ('꼭지', 'eb2f09a1de41412e8b2357bc04f26e74', EmojiCode.RED_CIRCLE)  # topic
     section_db = ('전개', '9f21ad86079d4caaa7ed9461a7f37288', EmojiCode.RED_HEART)
 
-    point_db = ('꼭지', '2c5411ba6a0f43a0a8aa06295751e37a', EmojiCode.PURPLE_CIRCLE)
+    topic_db = ('주제', '52d387ea0aaa470cb69332708c61b34d', EmojiCode.YELLOW_CIRCLE)  # asset
+    reading_db = ('읽기', 'c326f77425a0446a8aa309478767c85b', EmojiCode.YELLOW_HEART)
+
+    point_db = ('요점', '2c5411ba6a0f43a0a8aa06295751e37a', EmojiCode.PURPLE_CIRCLE)
     subject_db = ('담론', 'eca1ba6d4831459ca8becc283f1f8c4e', EmojiCode.PURPLE_HEART)
 
     date_db = ('일간', '961d1ca0a3d24a46b838ba85e710f18d', EmojiCode.GREEN_CIRCLE)
