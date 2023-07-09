101d3524ae04be5b5bf1b477198ae9d2fd44f88c
Younghoon YUN <dyhn78@gmail.com>
2023-07-10 01:59:18 +0900

    Update block_enum.py

--- workflow/constant/block_enum.py
+++ workflow/constant/block_enum.py
@@ -17,7 +17,7 @@ class DatabaseEnum(Enum):
 
     schedule_db = ('일정', 'e8782fe4e1a34c9d846d57b01a370327', EmojiCode.BLUE_CIRCLE)
 
-    event_db = ('일과', 'c8d46c01d6c941a9bf8df5d115a05f03', EmojiCode.ORANGE_CIRCLE)
+    event_db = ('일지', 'c8d46c01d6c941a9bf8df5d115a05f03', EmojiCode.ORANGE_CIRCLE)  # journal
     issue_db = ('줄기', 'e8782fe4e1a34c9d846d57b01a370327', EmojiCode.BLUE_HEART)
 
     journal_db = ('일지', 'c226cffe6cf84ab996bbc384bf26bf1d', EmojiCode.ORANGE_CIRCLE)  # DEPR
