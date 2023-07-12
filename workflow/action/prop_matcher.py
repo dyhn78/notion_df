eacf714e360c2a59e2ab9735a58ec4dc1ba9c1c3
Younghoon YUN <dyhn78@gmail.com>
2023-07-12 13:38:58 +0900

    Update prop_matcher.py

--- workflow/action/prop_matcher.py
+++ workflow/action/prop_matcher.py
@@ -273,6 +273,7 @@ class MatchTopic(MatchAction):
             if set(curr_topic_list) & ref_topic_set:
                 continue
             new_topic_set.update(ref_topic_set)
+        curr_topic_list = list(record.retrieve().properties[self.record_to_topic_prop])
         new_topic_set.difference_update(curr_topic_list)
         if not new_topic_set:
             return
