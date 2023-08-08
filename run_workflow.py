74cfa1a62451b2b0edfadb8e8a56135a6e9b12a0
dyhn78 <dyhn78@gmail.com>
2023-08-08 12:47:04 +0900

    revert temp disable

--- run_workflow.py
+++ run_workflow.py
@@ -1,5 +1,7 @@
+import os
 import sys
 from pathlib import Path
+from time import sleep
 
 import psutil
 
@@ -19,8 +21,6 @@ def is_already_running(script_path: Path):
 
 
 if __name__ == '__main__':
-    exit(0)
-
     this_path = Path(__file__).resolve()
     if is_already_running(this_path):
         sys.stderr.write("Script is already running.\n")
