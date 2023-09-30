b7a6d46369990bd16ac29bef77acae9125e6a005
Younghoon YUN <dyhn78@gmail.com>
2023-09-30 19:06:32 +0900

    Update block_enum.py

--- workflow/block_enum.py
+++ workflow/block_enum.py
@@ -19,12 +19,12 @@ class DatabaseEnum(Enum):
     stage_db = ('바탕', 'fa7d93f6fbd341f089b185745c834811', EmojiCode.BLUE_HEART)
 
     issue_db = ('줄기', 'e8782fe4e1a34c9d846d57b01a370327', EmojiCode.YELLOW_CIRCLE)
-    schedule_db = ('안배', 'addc94642ee74825bd31109f4fd1c9ee', EmojiCode.ORANGE_CIRCLE)
+    reading_db = ('읽기', 'c326f77425a0446a8aa309478767c85b', EmojiCode.YELLOW_HEART)
     
     topic_db = ('꼭지', 'eb2f09a1de41412e8b2357bc04f26e74', EmojiCode.RED_CIRCLE)
     point_db = ('요점', '2c5411ba6a0f43a0a8aa06295751e37a', EmojiCode.RED_HEART)
 
-    reading_db = ('읽기', 'c326f77425a0446a8aa309478767c85b', EmojiCode.YELLOW_HEART)
+    schedule_db = ('안배', 'addc94642ee74825bd31109f4fd1c9ee', EmojiCode.ORANGE_CIRCLE)
 
     date_db = ('일간', '961d1ca0a3d24a46b838ba85e710f18d', EmojiCode.GREEN_CIRCLE)
     week_db = ('주간', 'd020b399cf5947a59d11a0b9e0ea45d0', EmojiCode.GREEN_HEART)
