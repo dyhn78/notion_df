ec5dfe798e0f30ba71a2afe83f47ad89d2cc4afe
Zenbook <dyhn78@gmail.com>
2023-10-07 17:21:02 +0900

    fix dir structure

---
+++ deprecated/script/profile.ps1
@@ -0,0 +1,4 @@
+# put this script on $PSHOME
+
+"Activate notion_df\venv"
+C:\Workspace\notion_df\venv\Scripts\activate.ps1
--- script/profile.ps1
+++
@@ -1,4 +0,0 @@
-# put this script on $PSHOME
-
-"Activate notion_zap\venv"
-C:\Workspace\notion_zap\venv\Scripts\activate.ps1
