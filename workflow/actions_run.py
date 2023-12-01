5339a851f73ea249a81cc3e3a5a3439e6b6eea56
dyhn78 <dyhn78@gmail.com>
2023-12-01 17:16:56 +0900

    fix: actions_run code cleanup

--- workflow/actions_run.py
+++ workflow/actions_run.py
@@ -8,21 +8,15 @@ import psutil
 
 from workflow import actions, project_dir
 
-
-def is_already_running():
+if __name__ == '__main__':
     for process in psutil.process_iter(['name', 'cmdline']):
         try:
             if process.cmdline()[:2] == [sys.executable, actions.__file__]:
-                return True
+                sys.stderr.write(f"{actions.__name__} is already running.\n")
+                sys.exit(1)
         except (psutil.AccessDenied, psutil.NoSuchProcess):
             continue
-    return False
 
-
-if __name__ == '__main__':
-    if is_already_running():
-        sys.stderr.write(f"{actions.__name__} is already running.\n")
-        sys.exit(1)
     subprocess.run([sys.executable, actions.__file__],
                    env={**os.environ, 'PYTHONPATH': project_dir})
     sleep(5)
