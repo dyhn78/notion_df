19427512ceefe2f7a026151653e336b0f95e57ff
Younghoon YUN <dyhn78@gmail.com>
2023-12-02 20:12:01 +0900

    fix: actions_run module name

--- workflow/actions_run.py
+++ workflow/actions_run.py
@@ -21,4 +21,4 @@ if __name__ == '__main__':
                    env={**os.environ, 'PYTHONPATH': project_dir},
                    check=False)
     sleep(5)
-    os.execv(sys.executable, [sys.executable, Path(__file__).resolve()])
+    os.execv(sys.executable, [sys.executable, '-m', 'workflow.actions_run'])
