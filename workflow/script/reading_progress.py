3273894a318b5cd33a24b5d1704fcd850367f6a9
Zenbook <dyhn78@gmail.com>
2024-01-07 20:50:31 +0900

    fix: deduplicate

--- workflow/script/reading_progress.py
+++ workflow/script/reading_progress.py
@@ -4,6 +4,10 @@ from workflow.block_enum import DatabaseEnum
 
 if __name__ == '__main__':
     for event in DatabaseEnum.event_db.entity.query(event_to_reading_prog_prop.filter.is_not_empty()):
+        event_readings = event.data.properties[event_to_reading_prop]
+        event_readings_new = event_readings + event.data.properties[event_to_reading_prog_prop]
+        if event_readings == event_readings_new:
+            continue
         event.update(PageProperties({
             event_to_reading_prop: (event.data.properties[event_to_reading_prop]
                                     + event.data.properties[event_to_reading_prog_prop])
