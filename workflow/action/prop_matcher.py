c915aaf4335742f3036dca2668358538a24f593e
Zenbook <dyhn78@gmail.com>
2024-01-07 21:48:20 +0900

    fix: MatchEventProgress

--- workflow/action/prop_matcher.py
+++ workflow/action/prop_matcher.py
@@ -67,14 +67,14 @@ class MatchEventProgress(MatchAction):
                                    and event_to_reading_prog_prop.filter.is_empty())
 
     def _filter(self, event: Page) -> bool:
-        return (event.data.parent == self.event_db
-                and not event.data.properties[event_to_reading_prog_prop])
+        return event.data.parent == self.event_db
 
     def process_page(self, event: Page) -> Any:
         # TODO: more edge case handling
         if not (len(reading_list := event.data.properties[event_to_reading_prop]) == 1
                 and not event.data.properties[event_to_issue_prop]
-                and not event.data.properties[event_to_topic_prop]):
+                and not event.data.properties[event_to_topic_prop]
+                and not event.data.properties[event_to_reading_prog_prop]):
             logger.info(f'\t{event}\n\t\t-> :Skipped')
             return
         reading = reading_list[0]
