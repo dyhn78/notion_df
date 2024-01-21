44cb11ca0896d698835afbcf250ac03049245b26
Younghoon YUN <dyhn78@gmail.com>
2024-01-21 13:19:23 +0900

    chore: uncomment

--- workflow/block_enum.py
+++ workflow/block_enum.py
@@ -80,7 +80,4 @@ def is_template(page: Page) -> bool:
     database = page.data.parent
     if not database or not isinstance(database, Database):
         return False
-    # TODO: figure when isinstance(database.data.title.plain_text, str) wrongly set
-    # logger.critical(f'database.data.title - {database.data.title}')
-    # logger.critical(f'page.data.properties.title - {page.data.properties.title}')
     return bool(re.match(f'<{database.get_data().title.plain_text}> .*', page.data.properties.title.plain_text))
