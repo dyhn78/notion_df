eeb7620ca73707d2be1c8684c903add5e0d54cad
Zenbook <dyhn78@gmail.com>
2024-03-23 00:16:57 +0900

    chore: generated -> out

--- workflow/__init__.py
+++ workflow/__init__.py
@@ -2,11 +2,11 @@ from pathlib import Path
 
 _this = Path(__file__).resolve()
 project_dir = _this.parents[1]
-generated_dir = project_dir / 'generated'
-generated_dir.mkdir(exist_ok=True)
-backup_dir = generated_dir / 'backup'
+out_dir = project_dir / 'out'
+out_dir.mkdir(exist_ok=True)
+backup_dir = out_dir / 'backup'
 backup_dir.mkdir(exist_ok=True)
-log_dir = generated_dir / 'logs'
+log_dir = out_dir / 'logs'
 log_dir.mkdir(exist_ok=True)
-output_dir = generated_dir / 'output'  # for scripts
-output_dir.mkdir(exist_ok=True)
+etc_dir = out_dir / 'etc'  # for scripts
+etc_dir.mkdir(exist_ok=True)
