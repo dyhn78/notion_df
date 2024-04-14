26018089feff7c948d31eb36d185048882562933
Younghoon YUN <dyhn78@gmail.com>
2024-04-15 07:11:37 +0900

    Update match.py

--- workflow/action/match.py
+++ workflow/action/match.py
@@ -564,7 +564,7 @@ class DateINamespace(DatabaseNamespace):
         earliest_datei_date = min(datei_date_list)
         needs_separator: bool = not has_separator  # and cls._digit_pattern.match(title.plain_text))
         return RichText([TextSpan(
-            f"{earliest_datei_date.strftime('%y%m%d')}{'|' if needs_separator else ''} "),
+            f"{earliest_datei_date.strftime('%y%m%d')}{'|' if needs_separator else ''}{' ' if title.plain_text.startswith('|') else ''}"),
             *title])
 
 
