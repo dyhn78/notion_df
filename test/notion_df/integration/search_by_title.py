d99ed1a1b8964c29f6ce0030e5e452677bbdca83
dyhn78 <dyhn78@gmail.com>
2024-09-16 23:50:25 +0900

    test: search_by_title

--- test/notion_df/integration/search_by_title.py
+++ test/notion_df/integration/search_by_title.py
@@ -3,5 +3,6 @@ from pprint import pprint, pformat
 from notion_df.entity import search_by_title
 
 if __name__ == '__main__':
-    result = search_by_title("", None, 'descending', 10)[:10]
-    print(pformat(result))
+    result = search_by_title("", None, 'descending', page_size=5)
+    pprint(result)
+    pprint(result[:10])
