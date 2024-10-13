a9af63c2eeb56b1b804600ba65a5eeb83e2967df
dyhn78 <dyhn78@gmail.com>
2024-10-14 08:13:41 +0900

    chore: update my_block.py

--- app/my_block.py
+++ app/my_block.py
@@ -27,12 +27,12 @@ _entity_to_enum = {}
 class DatabaseEnum(Enum):
     event_db = ('일과', 'c8d46c01d6c941a9bf8df5d115a05f03', EmojiCode.BLUE_CIRCLE)
     journal_db = ('바탕', 'fa7d93f6fbd341f089b185745c834811', EmojiCode.BLUE_HEART)
-    area_db = ('사태', 'eb2f09a1de41412e8b2357bc04f26e74', EmojiCode.GREEN_CIRCLE)
-    thread_db = ('줄기', 'addc94642ee74825bd31109f4fd1c9ee', EmojiCode.GREEN_HEART)
+    area_db = ('사태', 'eb2f09a1de41412e8b2357bc04f26e74', EmojiCode.RED_CIRCLE)
+    thread_db = ('줄기', 'addc94642ee74825bd31109f4fd1c9ee', EmojiCode.RED_HEART)
     stage_db = ('수행', 'e8782fe4e1a34c9d846d57b01a370327', EmojiCode.YELLOW_CIRCLE) 
     reading_db = ('읽기', 'c326f77425a0446a8aa309478767c85b', EmojiCode.YELLOW_HEART)
-    idea_db = ('꼭지', '52d387ea0aaa470cb69332708c61b34d', EmojiCode.RED_CIRCLE)
-    resource_db = ('기술', '2c5411ba6a0f43a0a8aa06295751e37a', EmojiCode.RED_HEART)
+    idea_db = ('꼭지', '52d387ea0aaa470cb69332708c61b34d', EmojiCode.GREEN_CIRCLE)
+    resource_db = ('기술', '2c5411ba6a0f43a0a8aa06295751e37a', EmojiCode.GREEN_HEART)
     datei_db = ('일간', '961d1ca0a3d24a46b838ba85e710f18d', EmojiCode.PURPLE_CIRCLE)
     weeki_db = ('주간', 'd020b399cf5947a59d11a0b9e0ea45d0', EmojiCode.PURPLE_HEART)
 
