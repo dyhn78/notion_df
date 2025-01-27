79e4e3ba5bb70a7e36c2d52b7f9ccd278cc44e84
Younghoon YUN <dyhn78@gmail.com>
2025-01-28 08:56:14 +0900

    Update my_block.py

--- app/my_block.py
+++ app/my_block.py
@@ -27,7 +27,7 @@ _entity_to_enum = {}
 class DatabaseEnum(Enum):
     journal_db = ('바탕', 'fa7d93f6fbd341f089b185745c834811', EmojiCode.BLUE_CIRCLE)
     event_db = ('일과', 'c8d46c01d6c941a9bf8df5d115a05f03', EmojiCode.BLUE_HEART)
-    thread_db = ('갈래', 'e8782fe4e1a34c9d846d57b01a370327', EmojiCode.RED_CIRCLE)
+    thread_db = ('안배', 'e8782fe4e1a34c9d846d57b01a370327', EmojiCode.RED_CIRCLE)
     stage_db = ('줄기', 'addc94642ee74825bd31109f4fd1c9ee', EmojiCode.RED_HEART)
     idea_db = ('꼭지', 'eb2f09a1de41412e8b2357bc04f26e74', EmojiCode.YELLOW_CIRCLE)
     reading_db = ('읽기', 'c326f77425a0446a8aa309478767c85b', EmojiCode.YELLOW_HEART)
